15378b490f2884b0ba37b0aeec0328fa799dd55c
tain316 <capton948@gmail.com>
2025-07-23 11:47:30 +0700

    Edit label age form Eng to label

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -19,11 +19,11 @@
         filled
         type="number"
         v-model="age"
-        label="Your age *"
+        label="อายุ *"
         lazy-rules
         :rules="[
-          val => val !== null && val !== '' || 'Please type your age',
-          val => val > 0 && val < 100 || 'Please type a real age'
+          val => val !== null && val !== '' || 'กรุณาพิมพ์อายุของคุณ',
+          val => val > 0 && val < 100 || 'กรุณาใส่อายุที่ถูกต้อง'
         ]"
       />
 
